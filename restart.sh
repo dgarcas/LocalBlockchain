386d0d25ee67db5a30d4ad46fc2bd90984008df8
dgarcas <david.garcia.castano@alumnos.upm.es>
2020-04-26 19:32:14 +0100

    Problemas con clique.getSigners()

--- restart.sh
+++ restart.sh
@@ -7,6 +7,16 @@ geth account new --datadir node1 --password pass
 geth account new --datadir node2 --password pass
 geth account new --datadir node3 --password pass
 
+cp pass ./node1
+
+ADD1=$(cat node1/keystore/* | awk -F\" '{print $4}')
+ADD2=$(cat node2/keystore/* | awk -F\" '{print $4}')
+ADD3=$(cat node3/keystore/* | awk -F\" '{print $4}')
+
+echo ADD1=$ADD1 > .env
+
+sed -e 's/address1/'"$ADD1"'/g' genesis-template.json | sed -e 's/address2/'"$ADD2"'/g' | sed -e 's/address3/'"$ADD3"'/g' > genesis.json
+
 geth init --datadir node1 genesis.json
 geth init --datadir node2 genesis.json
 geth init --datadir node3 genesis.json
